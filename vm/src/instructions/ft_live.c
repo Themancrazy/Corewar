b2a2d7b67c2f6d2855366bd7e474f3b0140cfddc
Themancrazy <ajanssen@live.fr>
2019-10-16 13:33:17 -0700

    implemented live

--- vm/src/instructions/ft_live.c
+++ vm/src/instructions/ft_live.c
@@ -5,7 +5,7 @@ void				ft_live(t_cw *cw, t_process **cp)
     (void)cw;
     (void)cp;
     t_process   *tcp;
-    // cw->winner = *(cp->id);
+    
     tcp = *cp;
     tcp->live_call = cw->cycle.cycle;
     cw->winner = tcp->id;
