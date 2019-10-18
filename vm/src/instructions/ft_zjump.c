14f4134a5d6d25e134d182262587eec9d00f974e
Themancrazy <ajanssen@live.fr>
2019-10-17 20:34:21 -0700

    kura add ncurses and wrong zjump

--- vm/src/instructions/ft_zjump.c
+++ vm/src/instructions/ft_zjump.c
@@ -4,5 +4,13 @@ void				ft_zjump(t_cw *cw, t_process *cp)
 {
     (void)cw;
     (void)cp;
+    uint32_t    jump_size;
+
+    jump_size = 0;
+    if (cp->carry == 1)
+    {
+        ft_memcpy((void *)&jump_size, cp->pc, 4);
+        cp->pc = (cp->pc + jump_size);
+    }
     printf("zjump called\n");
 }
